817eeb4ccc4bc41a597b26ef2c133161ca5f646e
Beth Dellea <bethdellea@gmail.com>
2022-11-30 23:25:59 -0500

    Updates to handle form submission
    Updates to main.py and html templates to handle form submission.

--- main.py
+++ main.py
@@ -3,11 +3,21 @@ from config import *
 from todo import *
 
 app = Flask(__name__)
+#key for flash messages
+app.config['SECRET_KEY'] = SECRET_FLASH_KEY
+
+success_message = False
 
 @app.route("/", methods=('GET', 'POST'))
 def index():
-    todoReturn = get_todo_content()
-    return render_template('index.html', openTasks=todoReturn)
+    if request.method == 'POST':
+        print("POST SUBMITTED      ")
+        form_data = dict(request.form)
+        changes_stored = update_todo_items(form_data)
+        if changes_stored:
+            success_message = True
+        else:
+            success_message = False
+    todo_return = get_todo_content()
+    return render_template('index.html', openTasks=todo_return)
 
-
-#escape() please and thank you
